496e8379eba69c89e4331d3cf99b7cd6312fa77d
comsystan <com.syst.an@gmail.com>
2021-03-24 17:59:34 +0100

    changed menu entry

--- sig-opener/src/main/java/at/csa/csaj/sig/open/SignalOpener.java
+++ sig-opener/src/main/java/at/csa/csaj/sig/open/SignalOpener.java
@@ -62,7 +62,7 @@ import javax.swing.UIManager;
  * The {@link run} method implements the computations.
  * </p>
  */
-@Plugin(type = Command.class, menuPath = "Plugins>ComsystanJ>Signal>Signal Opener")
+@Plugin(type = Command.class, menuPath = "Plugins>ComsystanJ>Signal>Signal opener")
 public class SignalOpener<T extends RealType<T>> implements Command {
 	
 	private static final String PLUGIN_LABEL = "Opens single or multiple signals";
