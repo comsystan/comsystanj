af87bd146bac994fc9e1c1ff5533d713f05f6393
comsystan <com.syst.an@gmail.com>
2022-03-03 15:14:02 +0100

    Changed ArrayList to double[]

--- commons/src/main/java/at/csa/csaj/commons/utils/DoubleSort.java
+++ commons/src/main/java/at/csa/csaj/commons/utils/DoubleSort.java
@@ -1,67 +1,44 @@
 package at.csa.csaj.commons.utils;
 
-import java.util.ArrayList;
 
 public class DoubleSort {
 	
-	int[] indices;
-	ArrayList<Double> arrayList;
-	
-	double[] doublesSorted;
-	
-	
-	/*
-	 * Constructor
-	 * @Param double[]
-	 */
-	public DoubleSort(double[] array) {
-		setDoubleArray(array);
-	}
+	int[]    indices;
+	double[] doubles;
+	double   doubleTemp;
+	int      indexTemp;
 	
 	/*
 	 * Constructor
 	 * @Param double[]
 	 */
-	public DoubleSort() {
-	
-	}
-	
-	public void setDoubleArray(double[] array) {
-		
-		indices = new int[array.length];
-		arrayList = new ArrayList<Double>();
-		for(int i = 0; i < array.length; i++) {			
-			arrayList.add(array[i]);
-			indices[i] = i;			
-		}	
-	}
-	
-	public double[] getSortedDoubles() {	
-		doublesSorted = new double[arrayList.size()];
-		for(int i = 0; i < doublesSorted.length; i++) {			
-			doublesSorted[i] = arrayList.get(i);
+	public DoubleSort(double[] ds) {
+		int N = ds.length;
+		doubles = ds;
+		indices = new int[N];
+		for (int i = 0; i < N; i++) {
+			indices[i] = i;	
 		}
-		return doublesSorted;
+		
+	    for(int i = 0; i < N; i++) {
+	        for(int j = i + 1; j < N; j++) {
+	            if(doubles[j] <= doubles[i]) {
+	                doubleTemp = doubles[i];
+	                doubles[i] = doubles[j];
+	                doubles[j] = doubleTemp;
+	                indexTemp  = indices[i];
+	                indices[i] = indices[j];
+	                indices[j] = indexTemp;
+	            }
+	        }
+	    }
+	}
+	
+	public double[] getSortedDoubles() {
+		return doubles;
 	}
 	
 	public int[] getSortedIndices() {		
 		return indices;
-	}
-	
-	public void sort() {	
-    	double temp_value;
-    	int temp_index;
-        for(int k = 0; k < arrayList.size(); k++) {
-            for(int l = k + 1; l < arrayList.size(); l++) {
-                if(arrayList.get(k) >= arrayList.get(l)) {
-                    temp_value = arrayList.get(k);
-                    arrayList.set(k, arrayList.get(l));
-                    arrayList.set(l, temp_value);
-                    temp_index = indices[k];
-                    indices[k] = indices[l];
-                    indices[l] = temp_index;
-                }
-            }
-        }
 	}
 }
