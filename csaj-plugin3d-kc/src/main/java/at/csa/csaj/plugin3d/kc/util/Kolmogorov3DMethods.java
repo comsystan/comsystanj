fd0184a12eda2a528a7b9a9190ab6a961069e410
comsystan <com.syst.an@gmail.com>
2022-11-25 13:58:05 +0100

    Added new 3D volume plugin Kolmogorov complexity

---
+++ csaj-plugin3d-kc/src/main/java/at/csa/csaj/plugin3d/kc/util/Kolmogorov3DMethods.java
@@ -0,0 +1,41 @@
+/*-
+ * #%L
+ * Project: ImageJ2 plugin for computing 3D Kolmogorov complexity and Logical depth.
+ * File: Kolmogorov3DMethods.java
+ * 
+ * $Id$
+ * $HeadURL$
+ * 
+ * This file is part of ComsystanJ software, hereinafter referred to as "this program".
+ * %%
+ * Copyright (C) 2022 Comsystan Software
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package at.csa.csaj.plugin3d.kc.util;
+
+public interface Kolmogorov3DMethods{
+
+
+
+	double[] calcResults();
+
+	void setResults(double[] results);
+
+	double[] getResults();
+	
+}
