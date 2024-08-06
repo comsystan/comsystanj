d0fe04a4f4f1aca86f39ded4f446be047865cb2d
comsystan <com.syst.an@gmail.com>
2024-08-06 11:36:10 +0200

    Edited maven-build.bat

--- maven-build.bat
+++ maven-build.bat
@@ -3,6 +3,10 @@ echo off
 @REM This executes CreateCommandFiles.java to generate ContextCommand files from all the InteractiveCommand Csaj plugins
 call mvn exec:java
 
+echo Part 1/2 finished
+pause
+
 @REM Usuall build
 call mvn clean install
+echo Part 2/2 finished
 pause
