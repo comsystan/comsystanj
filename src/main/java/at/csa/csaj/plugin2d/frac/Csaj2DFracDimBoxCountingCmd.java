4df42666b848e27d928e474fe3f4d8e77275ac97
comsystan <com.syst.an@gmail.com>
2024-11-18 16:13:45 +0100

    Updated file headers

--- src/main/java/at/csa/csaj/plugin2d/frac/Csaj2DFracDimBoxCountingCmd.java
+++ src/main/java/at/csa/csaj/plugin2d/frac/Csaj2DFracDimBoxCountingCmd.java
@@ -1,7 +1,7 @@
 /*-
  * #%L
  * Project: ImageJ2/Fiji plugins for complex analyses of 1D signals, 2D images and 3D volumes
- * File: Csaj2DFracDimBoxCountingCommand.java
+ * File: Csaj2DFracDimBoxCountingCmd.java
  * 
  * $Id$
  * $HeadURL$
